bc13d6e13672e5891f64efd5742f1fc4cd6f4f66
André Castro <142420463+andrecastrom06@users.noreply.github.com>
2024-09-25 13:51:04 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+package br.edu.cesarschool.poo.cc.barbeiro;
+
+public class Pessoa {
+    private int id; // ID DA PESSOA SEJA BARBEIRO OU CLIENTE
+
+    public Pessoa(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id; // DEFINE O ID DA PESSOA
+    }
+}
